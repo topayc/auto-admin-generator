0e05a27b6c4aef6cc7ff9fb6514d2e0b7cacf1a3
topayc <topayc@naver.com>
2025-07-16 18:36:14 +0900

    item delete

--- ahn/src/main/java/com/databasemeta/ahn/controller/api/ApiController.java
+++ ahn/src/main/java/com/databasemeta/ahn/controller/api/ApiController.java
@@ -6,9 +6,14 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.databasemeta.ahn.dto.ApiResponse;
 import com.databasemeta.ahn.service.MainService;
 import com.databasemeta.ahn.service.SessionManager;
 import com.databasemeta.ahn.session.SessionInfo;
@@ -20,12 +25,13 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class ApiController {
   private final MainService mainService;
   private final SessionManager sessionManager;
 
   
-  @GetMapping(value = "/api") 
+  @GetMapping(value = "/") 
   public ResponseEntity<?> main(HttpServletRequest request, HttpServletResponse response ) throws Exception{
     HttpSession session = request.getSession(); 
     SessionInfo sessionInfo = this.sessionManager.checkSession(session);
@@ -37,7 +43,7 @@ public class ApiController {
     return ResponseEntity.ok().body(this.mainService.getMysqlDatabaseMetaData(sessionInfo.getDatasource()));
   }
 
-  @GetMapping(value = "/api/constraint") 
+  @GetMapping(value = "/constraint") 
   public ResponseEntity<?> constraint(String table, String constraint, HttpServletRequest request ,  HttpServletResponse response) throws Exception{
     HttpSession session = request.getSession(); 
     SessionInfo sessionInfo = this.sessionManager.checkSession(session);
@@ -49,5 +55,29 @@ public class ApiController {
     List<Map<String, Object>> constraintList = this.mainService.getConstraintData(sessionInfo,table, constraint);
     String html = TagGenerator.generateListTag(table, constraintList);
     return ResponseEntity.ok().body(html);
+  }
+
+  @DeleteMapping(value = "/items/{itemId}") 
+  public ResponseEntity<?> removeItem(
+    @RequestParam("tb") String table, @PathVariable("itemId") String itemId, 
+    HttpServletRequest request ,  
+    HttpServletResponse response) throws Exception{
+
+    HttpSession session = request.getSession(); 
+    SessionInfo sessionInfo = this.sessionManager.checkSession(session);
+    if (sessionInfo == null){
+      org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+      headers.setLocation(URI.create("http://localhost:8080/service/create"));
+      return new ResponseEntity<Void>(headers, HttpStatus.FOUND);
+    }
+    System.out.println("삭제 테이블 : " + table);
+    System.out.println("삭제 아이디 : " + itemId);
+
+    ApiResponse<String> apiResponse = ApiResponse.<String>builder()
+      .status("SUCCESS")
+      .message("요청이 성공적으로 처리되었습니다.")
+      .data("삭제 요청이 성공했습니다.")
+      .build();
+    return ResponseEntity.ok().body(apiResponse);
   }
 }
